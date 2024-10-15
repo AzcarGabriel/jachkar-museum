AzcarGabriel/jachkar-museum

commit 1c6ba58464c49062465dd269d0f0c7bee2556636

Fix version change bugs

Assets/Scripts/ScenesScripts/StoneDetailsScript.cs
@@ -72,8 +72,10 @@ public class StoneDetailsScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // NetworkManager.Singleton.SceneManager.LoadScene(StaticValues.previos_scene, LoadSceneMode.Single);
+            StaticValues.should_lock = true;
+            StaticValues.should_enable = true;
             SceneManager.UnloadSceneAsync("StoneDetails");
+            
         }
     }
 

Assets/Scripts/SmoothCameraPositionChange.cs
@@ -44,6 +44,12 @@ public class SmoothCameraPositionChange : MonoBehaviour
         RaycastHit hit;
         Vector3 pond = new Vector3(0.0f, 1.0f, 20.0f);
 
+        if (StaticValues.should_enable) 
+        {
+            StaticValues.should_enable = false;
+            fps.enabled = true;
+        }
+
         if (Input.GetKeyDown("e") && !fps.enabled)
         {
             targetGameObject = null;
@@ -67,7 +73,6 @@ public class SmoothCameraPositionChange : MonoBehaviour
                     StaticValues.stone_name = name;
                     // mainCamera.SetActive(false);
                     SceneManager.LoadScene("StoneDetails", LoadSceneMode.Additive);
-                    // NetworkManager.Singleton.SceneManager.LoadScene("StoneDetails", LoadSceneMode.Single);
                 }
             }
             else

Assets/Prefabs/Chat/ChatCanvas.prefab.meta
@@ -1,0 +1,7 @@
+fileFormatVersion: 2
+guid: be04bb188e5cfa24393e748900eaa664
+PrefabImporter:
+  externalObjects: {}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 

commit a9cd4c845ab57995b6afd8c641770938ab4c2a6b

fix: bug that duplicate details stone when is closed by pressing "e"
bug introduced in the commit 1c6ba58464c49062465dd269d0f0c7bee2556636

Assets/Scripts/ScenesScripts/StoneDetailsScript.cs
@@ -72,13 +72,12 @@ namespace ScenesScripts
                 }
             }
 
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.E))
             {
                 StaticValues.ShouldLock = true;
                 StaticValues.ShouldEnable = true;
                 Destroy(stone);
                 SceneManager.UnloadSceneAsync("StoneDetails");
-            
             }
         }
 

Assets/Scripts/SmoothCameraPositionChange.cs
@@ -44,12 +44,6 @@ public class SmoothCameraPositionChange : MonoBehaviour
         RaycastHit hit;
         Vector3 pond = new Vector3(0.0f, 1.0f, 20.0f);
 
-        if (StaticValues.ShouldEnable) 
-        {
-            StaticValues.ShouldEnable = false;
-            fps.enabled = true;
-        }
-
         if (Input.GetKeyDown("e") && !fps.enabled)
         {
             targetGameObject = null;